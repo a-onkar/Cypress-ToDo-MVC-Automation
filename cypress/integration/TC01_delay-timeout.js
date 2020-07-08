caf948678b2110cab20525f7d95b5aded3789013
a-onkar <mailanandteerth@gmail.com>
2020-07-08 13:54:42 +0100

    Updated

--- cypress/integration/TC01_delay-timeout.js
+++
@@ -1,8 +0,0 @@
-
-
-it('Adding delay and time-out', ()=>{
-    cy.visit('/?delay-new-todo=5000') // delays the elemtn with classname to appear
-    
-    cy.get('.new-todo',{timeout:6000}).type('Learn JavaScript {enter}')
-
-})
